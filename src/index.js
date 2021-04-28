bfac46d622ecc612cc6c89e90d0b3f34e0d77e58
Ismat <kamilov.ismet@gmail.com>
2021-04-29 02:36:22 +0400

    creating some shapes

--- src/index.js
+++ src/index.js
@@ -2,4 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Rectangle extends React.Component {
+    render() {
+        return (
+            <div className="rectangle">
+                <div className="rectangle-title">Upload your image</div>
+                <div className="file-format-helper">File should be Jpeg, Png,...</div>
+                <DragDropRectangle/>
+            </div>
+        )
+    }
+}
 
+function DragDropRectangle() {
+    return (
+        <div className="drag-drop-rectangle">
+            <svg className='rectangle-vector'>
+                <svg>
+                    <polygon className="plo"/>
+                </svg>
+            </svg>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <Rectangle/>,
+    document.getElementById("root")
+);
